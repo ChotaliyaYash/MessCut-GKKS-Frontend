e5b052557dc4fb13ca08c0d52b17e9add0b42723
Yash Chotaliya <yashchotaliya@192.168.1.5>
2023-11-01 21:37:45 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                GKKS
+            </div>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Cut Form</li>
+                    <li>Home</li>
+                    <li>Home</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header
